27cf14bac51320f075e020da507d0308617bf9e7
annugoyall <70103371+annugoyall@users.noreply.github.com>
2023-09-10 20:42:23 +0530

    Time: 43 ms (33.33%) | Memory: 16.5 MB (66.67%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -3,19 +3,25 @@ class Solution:
         if sx == fx and sy == fy:
             return t != 1
 
-        minimum_diagonally_step = min(abs(fx-sx), abs(fy-sy))
-        req_steps = minimum_diagonally_step
-        if minimum_diagonally_step == abs(fy-sy):
-            if sx < fx:
-                sx += minimum_diagonally_step
-            else:
-                fx += minimum_diagonally_step
-            req_steps += abs(fx-sx)
-        else:
-            if sy < fy:
-                sy += minimum_diagonally_step
-            else:
-                fy += minimum_diagonally_step
-            req_steps += abs(fy-sy)
+        x_dis = abs(sx-fx)
+        y_dis = abs(sy-fy)
+        steps = min(x_dis, y_dis) + abs(x_dis - y_dis)
+        return steps <= t
 
-        return req_steps <= t
+
+        # minimum_diagonally_step = min(abs(fx-sx), abs(fy-sy))
+        # req_steps = minimum_diagonally_step
+        # if minimum_diagonally_step == abs(fy-sy):
+        #     if sx < fx:
+        #         sx += minimum_diagonally_step
+        #     else:
+        #         fx += minimum_diagonally_step
+        #     req_steps += abs(fx-sx)
+        # else:
+        #     if sy < fy:
+        #         sy += minimum_diagonally_step
+        #     else:
+        #         fy += minimum_diagonally_step
+        #     req_steps += abs(fy-sy)
+
+        # return req_steps <= t
